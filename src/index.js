c01960424382f59dd2aeb5d1a66f50acfd5cf7d2
meganbachelor <meganbachelor@outlook.com>
2024-07-27 06:46:02 +0200

    Weather API integration
    Current function only working for the current temperature of the selected city. Not the additional attributes or forecast.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function getWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "58c32ct4af077777ac72ab80o05e54b7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(getWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
