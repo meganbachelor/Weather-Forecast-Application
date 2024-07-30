e902082578023a7146df56afa85eba4fe255e891
meganbachelor <meganbachelor@outlook.com>
2024-07-30 09:11:50 +0200

    Included more weather data
    excl. forecasts and icons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 function getWeather(response) {
   console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
+
   let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "58c32ct4af077777ac72ab80o05e54b7";
